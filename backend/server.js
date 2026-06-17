bbd181c1a9b13f1081d2c244ef98328e32a1ce55
Janani180506 <janani343434@gmail.com>
2026-06-17 12:25:24 +0530

    Updated Error

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
     cors({
         origin: [
-      "http://localhost:5001",
+      "http://localhost:5173",
       "https://atm-application-self.vercel.app"
     ],
     }));
